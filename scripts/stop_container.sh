3ced605899fa8e5ff298775b496f9c22c87446e2
SANKEERTHBD <124228824+SANKEERTHBD@users.noreply.github.com>
2024-09-16 03:36:09 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,9 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+if [ "$(docker ps -q -f name=flask-app-container)" ]; then
+    docker stop flask-app-container
+    docker rm flask-app-container
+else
+    echo "No container named 'flask-app-container' is running."
+fi
