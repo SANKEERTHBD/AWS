779bb528e91714a5b9029a424af561f9dced945f
SANKEERTHBD <124228824+SANKEERTHBD@users.noreply.github.com>
2024-09-16 03:35:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Pull the latest image from Docker Hub
 docker pull sankeerthbd/aws:latest
 
-docker run -d -p 5000:5000 sankeerthbd/aws
+# Stop and remove any existing container with the same name
+if [ "$(docker ps -q -f name=flask-app-container)" ]; then
+    docker stop flask-app-container
+    docker rm flask-app-container
+fi
+
+# Run a new container from the pulled image
+docker run -d -p 5000:5000 --name flask-app-container sankeerthbd/aws:latest
