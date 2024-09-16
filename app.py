fcab84b49719ccf26f42fdf7f710e64e8ad34ca7
SANKEERTHBD <124228824+SANKEERTHBD@users.noreply.github.com>
2024-09-16 15:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -262,8 +262,8 @@ def indexdata():
     #return render_template('register.html',data=msg)
     return resp
 
-if __name__=="__main__":
-    app.run(debug=True)     
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)  
     
 
 
